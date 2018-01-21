7d5dc98e1960d9eb4c41719ff6663c10473dbbab
william-crawford <will.crawford@live.com>
2018-01-21 13:18:19 -0500

    hello world

--- app/src/main/java/cs2340/edu/gatech/lamp/MainActivity.java
+++ app/src/main/java/cs2340/edu/gatech/lamp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Comment :P
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
