7ee3658a20224310a6e5a6db30fcd21a9763a840
Lausey7 <draegonblader7@gmail.com>
2018-01-21 18:32:36 -0500

    kk

--- app/src/main/java/cs2340/edu/gatech/lamp/MainActivity.java
+++ app/src/main/java/cs2340/edu/gatech/lamp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     // Comment :P
+    // buh.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
