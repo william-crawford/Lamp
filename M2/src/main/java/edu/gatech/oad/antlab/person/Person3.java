75c988d62184f107464cb22512d222314cf129d2
william-crawford <will.crawford@live.com>
2018-01-29 18:44:07 -0500

    fixed p3 infinite loop

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -44,7 +44,7 @@ public class Person3 {
 	private String calc(String input) {
 	  //Person 3 put your implementation here
 		String str = "";
-        for (int i = input.length() - 1; i >= 0; i++) {
+        for (int i = input.length() - 1; i >= 0; i--) {
             str += input.charAt(i);
         }
         return str;
