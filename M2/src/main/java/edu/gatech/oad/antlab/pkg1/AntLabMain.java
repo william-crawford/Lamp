c510ad8cb04044a9d94a2857fb355ecd47fc9994
Jermiah Russell <lorenzrussell@yahoo.com>
2018-01-30 01:02:42 -0500

    names added

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -58,7 +58,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person1 replace P1 with your name
 		  //and gburdell1 with your gt id
 		  Person1 p1 = new Person1("Jermiah Russell");
-		  toPrint += p1.toString("903184262") + "\n";
+		  toPrint += "\n" + p1.toString("903184262") + "\n";
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
 		  Person2 p2 = new Person2("Tucker von Holm");
@@ -69,11 +69,11 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p3.toString("gburdell3") + "\n";
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("P4");
+                  Person4 p4 = new Person4("Garrett Smiley");
                   toPrint += p4.toString("gburdell4") + "\n";
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
+                  Person5 p5 = new Person5("Lauren Chen");
                   toPrint += p5.toString("gburdell5") + "\n";
 		  
         System.out.println(toPrint);
