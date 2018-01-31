0f9281d9ec1e844ecc95ecdd114c431578a5be33
Garrett Smiley <garrettsmiley@msn.com>
2018-01-30 23:00:40 -0500

    please work now

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -4,7 +4,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  reversed string 
  *  
- *  @author  Bob
+ *  @author  Bob, Will Crawford
  *  @version 1.1
  */
 public class Person3 {
@@ -44,7 +44,7 @@ public class Person3 {
 	private String calc(String input) {
 	  //Person 3 put your implementation here
 		String str = "";
-        for (int i = input.length() - 1; i >= 0; i++) {
+        for (int i = input.length() - 1; i >= 0; i--) {
             str += input.charAt(i);
         }
         return str;
