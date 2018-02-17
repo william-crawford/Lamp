4b8eeec117014f3eae6ab15a158eeb545b78269a
william-crawford <will.crawford@live.com>
2018-02-17 16:55:11 -0500

    the map works

--- app/src/main/java/cs2340/edu/gatech/lamp/controller/MapsActivity.java
+++ app/src/main/java/cs2340/edu/gatech/lamp/controller/MapsActivity.java
@@ -41,8 +41,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(34, 84);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng culc = new LatLng(33.7749, -84.3964);
+        mMap.addMarker(new MarkerOptions().position(culc).title("Come sleep in the CULC!"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(culc));
     }
 }
