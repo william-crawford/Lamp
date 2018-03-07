2cd0f2eb95842b7c3d5041d0f890eda8b075cd53
Lauren Chen <draegonblader7@gmail.com>
2018-03-07 10:11:24 -0500

    details page from listview

--- app/src/main/java/cs2340/edu/gatech/lamp/controller/TestActivity.java
+++ app/src/main/java/cs2340/edu/gatech/lamp/controller/TestActivity.java
@@ -2,6 +2,7 @@ package cs2340.edu.gatech.lamp.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -9,7 +10,10 @@ import cs2340.edu.gatech.lamp.R;
 
 public class TestActivity extends AppCompatActivity {
 
-    TextView tv;
+    TextView name;
+    TextView addr;
+    TextView phone;
+    int sID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +21,21 @@ public class TestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_test);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Bundle bundle = getIntent().getExtras();
-        tv = (TextView) findViewById(R.id.textView3);
+        name = findViewById(R.id.textView2);
+        addr = findViewById(R.id.textView3);
+        phone = findViewById(R.id.textView4);
         if(bundle != null) {
-            tv.setText("You pressed: " + bundle.getString("testing"));
+            String title = bundle.getString("sName");
+            title += bundle.getBoolean("hasSpace") ? Html.fromHtml("<font color=\"#00CC00\">\t \u2714</font>"):
+                    Html.fromHtml("<color = \"#CC0000\"><b>\t\t\u2718</b></font>");
+            name.setText(title);
+            addr.setText(Html.fromHtml("<b>Address:</b> " + bundle.getString("sAddress")));
+            phone.setText(Html.fromHtml("<b>Phone:</b> " + bundle.getString("number")));
+            sID = bundle.getInt("id");
+        } else {
+            name.setText(Html.fromHtml("<b>Oops! This is awkward.</b>"));
+            addr.setText("");
+            phone.setText("");
         }
     }
 
