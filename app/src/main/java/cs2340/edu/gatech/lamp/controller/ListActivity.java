8911bfea430c09bb0c02261d5001e1b564f25be6
Jermiah Russell <lorenzrussell@yahoo.com>
2018-03-07 01:15:43 -0500

    switching

--- app/src/main/java/cs2340/edu/gatech/lamp/controller/ListActivity.java
+++ app/src/main/java/cs2340/edu/gatech/lamp/controller/ListActivity.java
@@ -23,7 +23,7 @@ import cs2340.edu.gatech.lamp.R;
  */
 
 public class ListActivity extends AppCompatActivity {
-//this
+//thiss
     Context context = this;
     ListView listView;
     ListAdapter listAdapter;
