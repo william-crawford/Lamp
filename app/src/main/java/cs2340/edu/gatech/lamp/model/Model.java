7bf1fa76479910ccaa9ee12673e65077549815a2
Tucker <icecream@vonholten.name>
2018-03-13 14:30:06 -0400

    Successful master merge in

--- app/src/main/java/cs2340/edu/gatech/lamp/model/Model.java
+++ app/src/main/java/cs2340/edu/gatech/lamp/model/Model.java
@@ -1,7 +1,12 @@
 package cs2340.edu.gatech.lamp.model;
 
+import android.content.Context;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import cs2340.edu.gatech.lamp.R;
 
 /**
  * Created by JermiahRussell on 2/13/2018.
@@ -14,37 +19,39 @@ public class Model {
         return _instance;
     }
 
-    private List<Shelter> allShelters = initShelters();
+    private List<Shelter> allShelters;
 
-
-    private Model() {
-    }
+    private Model() {}
 
     public List<Shelter> getAllShelters() {
         return allShelters;
     }
 
-    private List<Shelter> initShelters() {
-        List<Shelter> data = new ArrayList<>();
+    public void initShelters(Context context) {
+        allShelters = new ArrayList<>();
+        Scanner scan = new Scanner(context.getResources().openRawResource(R.raw.homeless_shelter_database));
+        scan.nextLine().split(","); //gets rid of the first line with names of columns
+        while (scan.hasNext()) {
+            String[] info = scan.nextLine().split(",");
+            allShelters.add(new Shelter(info));
+        }
 
-        data.add(new Shelter(
-                "The CULC",
-                new Location(33.7749, -84.3964, "266 4th St. NW"),
-                "7703643572",
-                "https://image.ibb.co/d3qhSc/CULC300x300.png",
-                3000,
-                150
-        ));
+//        data.add(new Shelter(
+//                "The CULC",
+//                new Location(33.7749, -84.3964, "266 4th St. NW"),
+//                false,
+//                "7703643572",
+//                "https://image.ibb.co/d3qhSc/CULC300x300.png"
+//        ));
+//
+//        data.add(new Shelter(
+//                "Architecture Roof",
+//                new Location(33.7761, -84.3960, "245 4th St. NW"),
+//                true,
+//                "5555555565",
+//                null
+//        ));
 
-        data.add(new Shelter(
-                "Architecture Roof",
-                new Location(33.7761, -84.3960, "245 4th St. NW"),
-                "5555555565",
-                null,
-                100,
-                20
-        ));
 
-        return data;
     }
 }
